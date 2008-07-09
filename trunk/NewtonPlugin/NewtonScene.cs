db27f6b0a51482f23fdfd671a3a01c1138392d44
wmfarr <wmfarr@ec5d9484-8850-0410-81ec-99a1d3d0ea3c>
2008-07-09 05:35:31 +0000

    Logs energy to console every 50 frames.

--- trunk/NewtonPlugin/NewtonScene.cs
+++ trunk/NewtonPlugin/NewtonScene.cs
@@ -27,26 +27,33 @@
 
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Axiom.Math;
 using Nini.Config;
 using OpenSim.Framework;
 using OpenSim.Region.Physics.Manager;
+using log4net;
 
 namespace OpenSim.Region.Physics.NewtonPlugin
 {
     public class NewtonScene : PhysicsScene
     {
+		private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private List<NewtonCharacter> _characters = new List<NewtonCharacter>();
         private List<NewtonPrim> _prims = new List<NewtonPrim>();
         private float[] _heightMap;
         private const float gravity = -9.8f;
+        private const float eps = 1.0f; // Softening distance. 
 
-        public NewtonScene()
+		private const int energyInterval = 50;
+		private int energyCounter = 0;
+		public NewtonScene()
         {
         }
 
         public override void Initialise(IMesher meshmerizer, IConfigSource config)
         {
+			
         }
 
         public override void Dispose()
@@ -121,14 +128,22 @@ namespace OpenSim.Region.Physics.NewtonPlugin
         /// </summary>
         private float distance2(PhysicsVector a, PhysicsVector b)
         {
-            return (float) (Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2) + Math.Pow(a.Z - b.Z, 2));
+          float dx = a.X - b.X;
+          float dy = a.Y - b.Y;
+          float dz = a.Z - b.Z;
+
+          // Math.Pow probably is a lot slower than dx*dx.
+          return dx*dx + dy*dy + dz*dz;
+          // return (float) (Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2) + Math.Pow(a.Z - b.Z, 2));
+        }
+
+        private float dot(PhysicsVector a, PhysicsVector b) 
+        {
+          return a.X*b.X + a.Y*b.Y + a.Z*b.Z;
         }
 
         private void calculateAcceleration(int i)
         {
-            // if prims are closer than this distance, gravitational forces between them are ignored
-            const float softeningDistance = 1.0f;
-
             PhysicsVector accel = new PhysicsVector();
 
             for (int j = 0; j < _prims.Count; ++j)
@@ -136,18 +151,77 @@ namespace OpenSim.Region.Physics.NewtonPlugin
                 if (i == j || !_prims[j].IsPhysical)
                     continue;
 
-                float dist2 = distance2(_prims[i].Position, _prims[j].Position);
-
-                if (dist2 < softeningDistance)
-                    continue;
-
                 PhysicsVector direction = _prims[j].Position - _prims[i].Position;
-                direction /= direction.length();
+                float dist2 = distance2(_prims[j].Position, _prims[i].Position);
+                float r = (float) Math.Sqrt(dist2 + eps*eps); // r is softened radius.
 
-                accel += Math.Abs(gravity) * _prims[j].Mass / dist2 * direction;
+                accel += Math.Abs(gravity) * _prims[j].Mass / (r*r*r) * direction;
             }
 
             _prims[i].SetAcceleration(accel);
+        }
+
+        private void calculateAccAndJerk(int i) {
+          PhysicsVector acc = new PhysicsVector();
+          PhysicsVector jerk = new PhysicsVector();
+
+          for (int j = 0; j < _prims.Count; j++) 
+            {
+              if (i == j || !_prims[j].IsPhysical)
+                continue;
+
+              PhysicsVector direction = _prims[j].Position - _prims[i].Position;
+              PhysicsVector velocity = _prims[j].Velocity - _prims[i].Velocity;
+              float dist2 = distance2(_prims[j].Position, _prims[i].Position);
+              float r = (float) Math.Sqrt(dist2 + eps*eps);
+              float r2 = r*r;
+              float r3 = r2*r;
+              float r5 = r2*r3;
+
+              acc += Math.Abs(gravity) * _prims[j].Mass * direction / r3;
+              // TODO: is dist2 ok here instead of distance?
+              // TODO: velocity/r3 is a vector, 3.0f*dot(direction, velocity)/r5 * dist2 is a scalar
+              // jerk += Math.Abs(gravity) * _prims[j].Mass * (velocity/r3 - 3.0f*dot(direction, velocity)/r5 * distance);
+            }
+
+          // TODO: enable once PhysicsActor has an overridable acceleration setter
+          // _prims[i].Acceleration = acc;
+          _prims[i].SetAcceleration(acc);
+          _prims[i].Jerk = jerk;
+        }
+
+        private void savePrimStates() 
+        {
+          for (int j = 0; j < _prims.Count; j++) 
+            {
+              _prims[j].SaveState();
+            }
+        }
+
+        private void predictPrims(float dt) 
+        {
+          float dt2 = dt*dt;
+          float dt3 = dt*dt2;
+
+          for (int j = 0; j < _prims.Count; j++)
+            {
+              _prims[j].Position += dt*_prims[j].Velocity + 0.5f*dt2*_prims[j].Acceleration + (1.0f/6.0f)*dt3*_prims[j].Jerk;
+              _prims[j].Velocity += dt*_prims[j].Acceleration + 0.5f*dt2*_prims[j].Jerk;
+            }
+        }
+
+        private void finishPrims(float dt) 
+        {
+          float dt2 = dt*dt;
+
+          for (int j = 0; j < _prims.Count; j++) 
+            {
+              // Important to do velocity first, so that more accurate velocity value can be used in position calculation.
+              _prims[j].Velocity = _prims[j].OldVelocity + 0.5f*(_prims[j].OldAcceleration + _prims[j].Acceleration)*dt + 
+                (1.0f/12.0f)*(_prims[j].OldJerk - _prims[j].Jerk)*dt2;
+              _prims[j].Position = _prims[j].OldPosition + 0.5f*(_prims[j].OldVelocity + _prims[j].Velocity)*dt + 
+                (1.0f/12.0f)*(_prims[j].OldAcceleration - _prims[j].Acceleration)*dt2;
+            }
         }
 
         private void updatePrimVelocities(float timestep)
@@ -232,20 +306,74 @@ namespace OpenSim.Region.Physics.NewtonPlugin
             }
         }
 
+        private float simulateCharacters(float timestep) 
+        {
+          float fps = 0;
+          for (int i = 0; i < _characters.Count; ++i) 
+          {
+            fps++;
+            updateCharacter(_characters[i], timestep);
+          }
+
+          return fps;
+        }
+
         public override float Simulate(float timestep)
         {
-            float fps = 0;
-            for (int i = 0; i < _characters.Count; ++i)
-            {
-                fps++;
-                
-                updateCharacter(_characters[i], timestep);
-            }
+			float fps = simulateCharacters(timestep);
+          
+			updatePrimVelocities(timestep);
+			updatePrimPositions(timestep);
+			if ((energyCounter++) % energyInterval == 0)
+			{
+				m_log.Info(Energy());
+				energyCounter = 0;
+			}
 
-            updatePrimVelocities(timestep);
-            updatePrimPositions(timestep);
+			
+			return fps;
+        }
 
-            return fps;
+        public float SimulateDKD(float timestep) 
+        {
+          float fps = simulateCharacters(timestep);
+
+          updatePrimPositions(0.5f*timestep);
+          updatePrimVelocities(timestep);
+          updatePrimPositions(0.5f*timestep);
+
+          return fps;
+        }
+
+        // This re-computes the force on each subsequent timestep:
+        // (KDK)(KDK)(KDK)....  Usually, you would store the results
+        // of the force computation, and re-use them in a
+        // "Kick-stored" advance so there is only one force
+        // computation per timestep on average: (KDK)(KsDK)(KsDK)....
+        public float SimulateKDK(float timestep)
+        {
+          float fps = simulateCharacters(timestep);
+
+          updatePrimVelocities(0.5f*timestep);
+          updatePrimPositions(timestep);
+          updatePrimVelocities(0.5f*timestep);
+
+          return fps;
+        }
+
+        public float SimulateHermite(float timestep) 
+        {
+          float fps = simulateCharacters(timestep);
+
+          // TODO: pass in int
+          // calculateAccAndJerk(timestep);
+          savePrimStates();
+          predictPrims(timestep);
+          // TODO: pass in int
+          // calculateAccAndJerk(timestep);
+          finishPrims(timestep);
+
+          return fps;
         }
 
         public override void GetResults()
@@ -271,6 +399,46 @@ namespace OpenSim.Region.Physics.NewtonPlugin
         {
             Dictionary<uint, float> returncolliders = new Dictionary<uint, float>();
             return returncolliders;
+        }
+
+        private float PairPotential(NewtonPrim p1, NewtonPrim p2) 
+        {
+          float d2 = distance2(p1.Position, p2.Position);
+          float r = (float) Math.Sqrt(d2 + eps*eps);
+
+          return -Math.Abs(gravity)*p1.Mass*p2.Mass/r;
+        }
+
+        public float KineticEnergy() 
+        {
+          float ke = 0.0f;
+
+          for (int i = 0; i < _prims.Count; i++) 
+            {
+              ke += _prims[i].KineticEnergy();
+            }
+
+          return ke;
+        }
+
+        public float PotentialEnergy() 
+        {
+          float pe = 0.0f;
+
+          for (int i = 0; i < _prims.Count; i++) 
+            {
+              for (int j = i+1; j < _prims.Count; j++) 
+                {
+                  pe += PairPotential(_prims[i], _prims[j]);
+                }
+            }
+
+          return pe;
+        }
+
+        public float Energy() 
+        {
+          return KineticEnergy() + PotentialEnergy();
         }
     }
 }
